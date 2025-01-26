5891d2bde755d842430ae9bc96df420f77180a42
Thomas Pendergrass <pendergrassthomas@gmail.com>
2025-01-26 13:09:38 -0700

    Add input delay and debounce

--- Assets/_Scripts/ScreenLoader.cs
+++ Assets/_Scripts/ScreenLoader.cs
@@ -4,14 +4,26 @@ using UnityEngine.SceneManagement;
 public class ScreenLoader : MonoBehaviour
 {
     public string sceneToLoad; 
+    private float startTime; 
+    private bool isLoading = false;
+
+    void Start()
+    {
+        startTime = Time.time; 
+    }
 
     void Update()
     {
-        // Check if any key is pressed
-        if (Input.anyKey)
+        // Check if the delay has passed, to avoid rage clicking through it
+        if (Time.time - startTime >= 1.2f) 
         {
-            // Load the specified scene
-            SceneManager.LoadScene(sceneToLoad);
+            // Check if any key is pressed
+            if (Input.anyKey && !isLoading)
+            {
+                isLoading = true;
+                // Load the specified scene
+                SceneManager.LoadScene(sceneToLoad);
+            }
         }
     }
 }
